0f1aabd6533c1145adf03cdc7468d1aeec249e47
Mohamed B Md Said <met.qwerty@gmail.com>
2021-05-16 23:56:32 +0800

    Initial Commit

---
+++ GitIgnore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitIgnore
+//
+//  Created by Mohamed bin Md Said on 16/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
